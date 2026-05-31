840b5ad248f7ef4873654bc32c544f20b845bf34
Alexey Samoylov <alexey.samoylov@gmail.com>
2026-05-31 19:23:49 +0600

    refactor: fold bus interface adapters into fx module

--- internal/apps/balda/eventbus/nats/fx.go
+++ internal/apps/balda/eventbus/nats/fx.go
@@ -1,16 +1,18 @@
 package natsbus
 
 import (
+	"github.com/normahq/balda/internal/apps/balda/swarm"
+	actorengine "github.com/normahq/norma/pkg/actorlayer/engine"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Module("balda_eventbus_nats",
 	fx.Provide(
 		NewBus,
-		newActorDispatcher,
-		newEventPublisher,
-		newBusDrainer,
-		newActorDeliverySource,
-		newEventConsumer,
+		func(bus *Bus) swarm.ActorDispatcher { return bus },
+		func(bus *Bus) swarm.EventPublisher { return bus },
+		func(bus *Bus) swarm.BusDrainer { return bus },
+		func(bus *Bus) actorengine.Source { return bus },
+		func(bus *Bus) swarm.EventConsumer { return bus },
 	),
 )
